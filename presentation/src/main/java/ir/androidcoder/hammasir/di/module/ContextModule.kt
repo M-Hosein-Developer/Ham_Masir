9bc8b6cafd5d1aa32321a7664ce3ebf64bb87247
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-07-26 05:08:34 -1200

    Setup Di

---
+++ presentation/src/main/java/ir/androidcoder/hammasir/di/module/ContextModule.kt
@@ -0,0 +1,23 @@
+package ir.androidcoder.hammasir.di.module
+
+import android.app.Application
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ContextModule {
+
+
+    @Provides
+    @Singleton
+    fun provideContext(application: Application): Context {
+        return application.applicationContext
+    }
+
+
+}
