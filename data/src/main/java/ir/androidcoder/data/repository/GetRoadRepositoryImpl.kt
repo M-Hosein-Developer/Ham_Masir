7cc5f4e403891ce4f1d905436b434e43d89ca600
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-08-06 22:47:37 -1200

    Create Get Road Repository

---
+++ data/src/main/java/ir/androidcoder/data/repository/GetRoadRepositoryImpl.kt
@@ -0,0 +1,10 @@
+package ir.androidcoder.data.repository
+
+import ir.androidcoder.data.remote.ApiService
+import ir.androidcoder.domain.repository.GetRoadRepository
+
+class GetRoadRepositoryImpl(val apiService: ApiService) : GetRoadRepository {
+
+
+
+}
---
+++ domain/src/main/java/ir/androidcoder/domain/repository/GetRoadRepository.kt
@@ -0,0 +1,7 @@
+package ir.androidcoder.domain.repository
+
+interface GetRoadRepository {
+
+
+
+}
