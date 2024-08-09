979ebe40128852a5c6ab3f1bf597f3a289de3ace
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-08-08 18:48:28 -1200

    Setup Data - RepositoryImpl

--- data/src/main/java/ir/androidcoder/data/repository/GetRoadRepositoryImpl.kt
+++ data/src/main/java/ir/androidcoder/data/repository/GetRoadRepositoryImpl.kt
@@ -1,10 +1,27 @@
 package ir.androidcoder.data.repository
 
+import ir.androidcoder.data.mapper.mapToEntity
 import ir.androidcoder.data.remote.ApiService
+import ir.androidcoder.domain.entities.RouteEntity
 import ir.androidcoder.domain.repository.GetRoadRepository
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class GetRoadRepositoryImpl(val apiService: ApiService) : GetRoadRepository {
 
+
+    override suspend fun getRoute(point1: String, point2: String): Flow<RouteEntity> = flow {
+
+        while (true){
+
+            val response = apiService.getRoute(key = "c5178a7a-f816-43d3-a1ba-d7434c8670f7" , point1 = point1 , point2 = point2)
+            emit(mapToEntity(response))
+            delay(2000)
+
+        }
+
+    }
 
 
 }
