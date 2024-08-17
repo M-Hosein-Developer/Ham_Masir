454ba36ccd7a4e74899040a557fd6f2584b9f0ec
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-08-16 20:59:24 -1200

    Add SearchTextField In Search Screen

--- presentation/src/main/java/ir/androidcoder/hammasir/screen/feature/SearchScreen.kt
+++ presentation/src/main/java/ir/androidcoder/hammasir/screen/feature/SearchScreen.kt
@@ -1,11 +1,81 @@
 package ir.androidcoder.hammasir.screen.feature
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.ArrowForward
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun SearchScreen(){
 
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
 
+        SearchTextField(
+            "در نقشه جسجتو کن",
+            {
+
+            },
+            {
+
+            }
+        )
+
+        LocationByCategory()
+        ImportantLocation()
+
+    }
+
+}
+
+@Composable
+fun SearchTextField(value: String, onValueChange: (String) -> Unit, onBackClick: () -> Unit) {
+
+    OutlinedTextField(
+        value = value,
+        onValueChange = { onValueChange.invoke(it) },
+        trailingIcon = {
+            Icon(
+                imageVector = Icons.AutoMirrored.Outlined.ArrowForward,
+                contentDescription = null,
+                modifier = Modifier.clickable {
+                    onBackClick.invoke()
+                }
+            )
+        },
+        modifier = Modifier.fillMaxWidth(),
+        textStyle = TextStyle(
+            textAlign = TextAlign.End
+        ),
+        shape = RoundedCornerShape(32.dp)
+    )
+
+}
+
+@Composable
+fun LocationByCategory() {
 
 
 }
+
+@Composable
+fun ImportantLocation() {
+
+
+}
+
