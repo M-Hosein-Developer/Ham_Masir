0a2b92aeaa1bc761406a8478a9c51bd40ba508d8
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-08-18 01:30:27 -1200

    Add Category Ui Feature

--- presentation/src/main/java/ir/androidcoder/hammasir/screen/feature/SearchScreen.kt
+++ presentation/src/main/java/ir/androidcoder/hammasir/screen/feature/SearchScreen.kt
@@ -1,31 +1,50 @@
 package ir.androidcoder.hammasir.screen.feature
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.ArrowForward
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import ir.androidcoder.hammasir.R
+import ir.androidcoder.hammasir.util.Category
 
 @Composable
 fun SearchScreen(navController: NavHostController) {
 
     var searchText by remember { mutableStateOf("") }
+
+    val categories = listOf(
+        Category("پمپ بنزین", R.drawable.gas_station),
+        Category("پمپ گاز", R.drawable.gas_station),
+        Category("رستوران", R.drawable.food),
+        Category("سرویس بهداشتی", R.drawable.wc),
+    )
 
     Column(
         Modifier
@@ -43,8 +62,29 @@ fun SearchScreen(navController: NavHostController) {
             }
         )
 
-        LocationByCategory()
-        ImportantLocation()
+
+        Column(
+            Modifier.fillMaxSize()
+                .verticalScroll(rememberScrollState())
+        ) {
+
+            LocationByCategory(categories) {
+
+                when(it){
+
+                    categories[0].categoryName -> {}
+                    categories[1].categoryName -> {}
+                    categories[2].categoryName -> {}
+                    categories[3].categoryName -> {}
+
+                }
+
+            }
+
+            ImportantLocation()
+
+        }
+
 
     }
 
@@ -54,16 +94,24 @@ fun SearchScreen(navController: NavHostController) {
 fun SearchTextField(value: String, onValueChange: (String) -> Unit, onBackClick: () -> Unit) {
 
     OutlinedTextField(
-        label = { Text("در نقشه جستجو کن") },
+        label = {
+            Text(
+                "در نقشه جستجو کن",
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.End
+            )
+        },
         value = value,
         onValueChange = { onValueChange.invoke(it) },
         trailingIcon = {
             Icon(
                 imageVector = Icons.AutoMirrored.Outlined.ArrowForward,
                 contentDescription = null,
-                modifier = Modifier.clickable {
-                    onBackClick.invoke()
-                }
+                modifier = Modifier
+                    .clickable {
+                        onBackClick.invoke()
+                    }
+                    .padding(end = 8.dp)
             )
         },
         modifier = Modifier.fillMaxWidth(),
@@ -76,8 +124,34 @@ fun SearchTextField(value: String, onValueChange: (String) -> Unit, onBackClick:
 }
 
 @Composable
-fun LocationByCategory() {
+fun LocationByCategory(categories: List<Category>, onItemClicked: (String) -> Unit) {
 
+    LazyRow(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = CenterVertically,
+        reverseLayout = true
+    ) {
+        items(categories.size) {
+            LocationByCategoryItem(categories[it]) { onItemClicked.invoke(it) }
+        }
+    }
+
+}
+
+@Composable
+fun LocationByCategoryItem(category: Category, onItemClicked: (String) -> Unit) {
+
+    TextButton(
+        onClick = { onItemClicked.invoke(category.categoryName) },
+        modifier = Modifier
+            .padding(horizontal = 4.dp)
+            .padding(vertical = 14.dp)
+            .border(1.dp, Color.Gray, RoundedCornerShape(32.dp)),
+    ) {
+        Text(text = category.categoryName)
+        Spacer(modifier = Modifier.width(8.dp))
+        Icon(painter = painterResource(id = category.icon), contentDescription = null)
+    }
 
 }
 
