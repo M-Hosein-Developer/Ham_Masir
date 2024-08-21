93621613cc12ec294c8a6a3c8a101ee918a725ad
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-08-20 19:07:03 -1200

    Create Search And Home Mapper

--- data/src/main/java/ir/androidcoder/data/mapper/SearchMapper.kt
+++ data/src/main/java/ir/androidcoder/data/mapper/SearchMapper.kt
@@ -1,2 +1,25 @@
 package ir.androidcoder.data.mapper
 
+import ir.androidcoder.domain.entities.HomeEntity
+
+fun HomeEntity.toHomeData() = ir.androidcoder.data.local.entities.HomeEntity(
+
+    id = id,
+    latitude = latitude,
+    longitude = longitude,
+    name = name,
+    address = address,
+    city = city
+
+)
+
+fun ir.androidcoder.data.local.entities.HomeEntity.toHomeDomain() = HomeEntity(
+
+    id = id,
+    latitude = latitude,
+    longitude = longitude,
+    name = name,
+    address = address,
+    city = city
+
+)
