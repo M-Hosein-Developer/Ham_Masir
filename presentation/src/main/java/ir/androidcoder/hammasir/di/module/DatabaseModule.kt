294ef874b57bb7e3d585323ac3cfee0cfa64479c
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-08-20 19:23:48 -1200

    Provide Database Module

---
+++ presentation/src/main/java/ir/androidcoder/hammasir/di/module/DatabaseModule.kt
@@ -0,0 +1,29 @@
+package ir.androidcoder.hammasir.di.module
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import ir.androidcoder.data.local.MyDatabase
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase (@ApplicationContext context: Context) : MyDatabase = Room
+        .databaseBuilder(context , MyDatabase::class.java , "MyDatabase")
+        .fallbackToDestructiveMigration()
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideDao (db : MyDatabase) = db.myDao()
+
+
+}
