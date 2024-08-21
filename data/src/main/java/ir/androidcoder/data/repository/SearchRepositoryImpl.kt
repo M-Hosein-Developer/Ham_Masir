2ef92add7c00e9a1978a2584259b6bcb7c48a819
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-08-20 19:07:30 -1200

    Create Search Repository

--- data/src/main/java/ir/androidcoder/data/repository/SearchRepositoryImpl.kt
+++ data/src/main/java/ir/androidcoder/data/repository/SearchRepositoryImpl.kt
@@ -1,4 +1,17 @@
 package ir.androidcoder.data.repository
 
-class SearchRepositoryImpl {
+import ir.androidcoder.data.local.MyDao
+import ir.androidcoder.data.mapper.toHomeData
+import ir.androidcoder.data.mapper.toHomeDomain
+import ir.androidcoder.domain.entities.HomeEntity
+import ir.androidcoder.domain.repository.SearchRepository
+
+class SearchRepositoryImpl(private val dao: MyDao) : SearchRepository {
+
+    //Home Location
+    override suspend fun insertHomeLocation(homeLocation: HomeEntity) = dao.insertHomeItem(homeLocation.toHomeData())
+
+    override suspend fun getHomeLocation(): HomeEntity = dao.getHomeItems().toHomeDomain()
+
+
 }
--- domain/src/main/java/ir/androidcoder/domain/repository/SearchRepository.kt
+++ domain/src/main/java/ir/androidcoder/domain/repository/SearchRepository.kt
@@ -1,4 +1,12 @@
 package ir.androidcoder.domain.repository
 
+import ir.androidcoder.domain.entities.HomeEntity
+
 interface SearchRepository {
+
+    //home location
+    suspend fun insertHomeLocation(homeLocation : HomeEntity)
+    suspend fun getHomeLocation() : HomeEntity
+
+
 }
