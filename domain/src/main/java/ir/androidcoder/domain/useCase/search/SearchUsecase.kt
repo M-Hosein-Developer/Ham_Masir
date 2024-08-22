e86fe185fbbbeb281f9a191ecfd54b37b3f8580c
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-08-21 18:39:27 -1200

    Setup Search usecase

--- domain/src/main/java/ir/androidcoder/domain/useCase/search/SearchUsecase.kt
+++ domain/src/main/java/ir/androidcoder/domain/useCase/search/SearchUsecase.kt
@@ -1,4 +1,11 @@
 package ir.androidcoder.domain.useCase.search
 
+import ir.androidcoder.domain.entities.HomeEntity
+
 interface SearchUsecase {
+
+    //home location
+    suspend fun insertHomeLocation(homeLocation : HomeEntity)
+    suspend fun getHomeLocation() : HomeEntity
+
 }
--- domain/src/main/java/ir/androidcoder/domain/useCase/search/SearchUsecaseImpl.kt
+++ domain/src/main/java/ir/androidcoder/domain/useCase/search/SearchUsecaseImpl.kt
@@ -1,4 +1,12 @@
 package ir.androidcoder.domain.useCase.search
 
-class SearchUsecaseImpl {
+import ir.androidcoder.domain.entities.HomeEntity
+import ir.androidcoder.domain.repository.SearchRepository
+
+class SearchUsecaseImpl(private val searchRepository: SearchRepository) : SearchUsecase {
+
+    override suspend fun insertHomeLocation(homeLocation: HomeEntity) = searchRepository.insertHomeLocation(homeLocation)
+
+    override suspend fun getHomeLocation(): HomeEntity = searchRepository.getHomeLocation()
+
 }
