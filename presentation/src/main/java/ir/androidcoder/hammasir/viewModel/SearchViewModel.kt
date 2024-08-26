97ad7c91b5be5b4ddb80671683bbe15bd0730b43
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-08-25 19:52:44 -1200

    Setup View Model For Work

--- presentation/src/main/java/ir/androidcoder/hammasir/viewModel/SearchViewModel.kt
+++ presentation/src/main/java/ir/androidcoder/hammasir/viewModel/SearchViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import ir.androidcoder.domain.entities.HomeEntity
+import ir.androidcoder.domain.entities.WorkEntity
 import ir.androidcoder.domain.useCase.search.SearchUsecase
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -13,9 +14,15 @@ import javax.inject.Inject
 @HiltViewModel
 class SearchViewModel @Inject constructor(private val usecase: SearchUsecase) : ViewModel() {
 
+    //home location
     private val _homeLocation = MutableLiveData<HomeEntity>()
     val homeLocation: LiveData<HomeEntity> = _homeLocation
 
+    //work location
+    private val _workLocation = MutableLiveData<WorkEntity>()
+    val workLocation : LiveData<WorkEntity> = _workLocation
+
+    //---home location------------------------------------------------------------------------------
     fun insertHomeLocation(homeLocation : HomeEntity) = viewModelScope.launch {
         usecase.insertHomeLocation(homeLocation)
     }
@@ -23,5 +30,15 @@ class SearchViewModel @Inject constructor(private val usecase: SearchUsecase) : 
     fun getHomeLocation() = viewModelScope.launch {
         _homeLocation.value = usecase.getHomeLocation()
     }
+
+    //---work location------------------------------------------------------------------------------
+    fun insertWorkLocation(workLocation : WorkEntity) = viewModelScope.launch {
+        usecase.insertWorkLocation(workLocation)
+    }
+
+    fun getWorkLocation() = viewModelScope.launch {
+        _workLocation.value = usecase.getWorkLocation()
+    }
+
 
 }
