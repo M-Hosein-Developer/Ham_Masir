3111b223186fc816c4154ca139e0d1ee2d852a23
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-08-25 19:13:50 -1200

    Setup Usecase fFor Work Location

--- domain/src/main/java/ir/androidcoder/domain/useCase/search/SearchUsecase.kt
+++ domain/src/main/java/ir/androidcoder/domain/useCase/search/SearchUsecase.kt
@@ -1,11 +1,16 @@
 package ir.androidcoder.domain.useCase.search
 
 import ir.androidcoder.domain.entities.HomeEntity
+import ir.androidcoder.domain.entities.WorkEntity
 
 interface SearchUsecase {
 
     //home location
     suspend fun insertHomeLocation(homeLocation : HomeEntity)
     suspend fun getHomeLocation() : HomeEntity
+
+    //work location
+    suspend fun insertWorkLocation(workEntity: WorkEntity)
+    suspend fun getWorkLocation() : WorkEntity
 
 }
--- domain/src/main/java/ir/androidcoder/domain/useCase/search/SearchUsecaseImpl.kt
+++ domain/src/main/java/ir/androidcoder/domain/useCase/search/SearchUsecaseImpl.kt
@@ -1,12 +1,19 @@
 package ir.androidcoder.domain.useCase.search
 
 import ir.androidcoder.domain.entities.HomeEntity
+import ir.androidcoder.domain.entities.WorkEntity
 import ir.androidcoder.domain.repository.SearchRepository
 
 class SearchUsecaseImpl(private val searchRepository: SearchRepository) : SearchUsecase {
 
+    //home location
     override suspend fun insertHomeLocation(homeLocation: HomeEntity) = searchRepository.insertHomeLocation(homeLocation)
 
     override suspend fun getHomeLocation(): HomeEntity = searchRepository.getHomeLocation()
+
+    //work location
+    override suspend fun insertWorkLocation(workEntity: WorkEntity) = searchRepository.insertWorkLocation(workEntity)
+
+    override suspend fun getWorkLocation(): WorkEntity = searchRepository.getWorkLocation()
 
 }
