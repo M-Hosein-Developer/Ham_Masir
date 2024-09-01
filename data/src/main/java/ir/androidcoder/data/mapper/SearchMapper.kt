2f25a830e029aea255e6edd8f847f009a9dd1cd8
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-08-31 22:14:10 -1200

    Setup Search Location Mapper

--- data/src/main/java/ir/androidcoder/data/mapper/SearchMapper.kt
+++ data/src/main/java/ir/androidcoder/data/mapper/SearchMapper.kt
@@ -1,6 +1,7 @@
 package ir.androidcoder.data.mapper
 
 import ir.androidcoder.domain.entities.HomeEntity
+import ir.androidcoder.domain.entities.SearchEntity
 import ir.androidcoder.domain.entities.WorkEntity
 
 //Home Mapper
@@ -48,3 +49,31 @@ fun ir.androidcoder.data.local.entities.WorkEntity.toWorkDomain() = WorkEntity(
     city = city
 
 )
+
+//Search Mapper
+fun SearchEntity.toSearchData() = ir.androidcoder.data.local.entities.SearchEntity(
+
+    id = id,
+    latitude = latitude,
+    longitude = longitude,
+    name = name,
+    address = address,
+    category = category,
+    city = city,
+    phone = phone
+
+)
+
+fun List<ir.androidcoder.data.local.entities.SearchEntity>.toSearchDomainList(): List<SearchEntity> =
+    this.map { searchEntity ->
+        SearchEntity(
+            id = searchEntity.id,
+            latitude = searchEntity.latitude,
+            longitude = searchEntity.longitude,
+            name = searchEntity.name,
+            address = searchEntity.address,
+            category = searchEntity.category,
+            city = searchEntity.city,
+            phone = searchEntity.phone
+        )
+    }
