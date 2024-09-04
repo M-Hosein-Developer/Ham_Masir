b958243c5a97d9be51b6e1346c75121a81f23d24
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-09-04 06:26:07 -1200

    Setup Search Mapper

--- data/src/main/java/ir/androidcoder/data/mapper/SearchMapper.kt
+++ data/src/main/java/ir/androidcoder/data/mapper/SearchMapper.kt
@@ -1,5 +1,6 @@
 package ir.androidcoder.data.mapper
 
+import ir.androidcoder.data.model.SearchResponse
 import ir.androidcoder.domain.entities.HomeEntity
 import ir.androidcoder.domain.entities.SearchEntity
 import ir.androidcoder.domain.entities.WorkEntity
@@ -51,29 +52,50 @@ fun ir.androidcoder.data.local.entities.WorkEntity.toWorkDomain() = WorkEntity(
 )
 
 //Search Mapper
-fun SearchEntity.toSearchData() = ir.androidcoder.data.local.entities.SearchEntity(
+fun SearchResponse.toSearchEntity(): SearchEntity {
+    return SearchEntity(
+        hits = this.hits.map { hit ->
+            SearchEntity.Hit(
+                city = hit.city,
+                country = hit.country,
+                countrycode = hit.countrycode,
+                extent = hit.extent.toList(),
+                name = hit.name,
+                osm_id = hit.osm_id,
+                osm_key = hit.osm_key,
+                osm_type = hit.osm_type,
+                osm_value = hit.osm_value,
+                point = SearchEntity.Hit.Point(
+                    lat = hit.point.lat,
+                    lng = hit.point.lng
+                ),
+                state = hit.state
+            )
+        },
+        locale = this.locale
+    )
+}
 
-    id = id,
-    latitude = latitude,
-    longitude = longitude,
-    name = name,
-    address = address,
-    category = category,
-    city = city,
-    phone = phone
-
-)
-
-fun List<ir.androidcoder.data.local.entities.SearchEntity>.toSearchDomainList(): List<SearchEntity> =
-    this.map { searchEntity ->
-        SearchEntity(
-            id = searchEntity.id,
-            latitude = searchEntity.latitude,
-            longitude = searchEntity.longitude,
-            name = searchEntity.name,
-            address = searchEntity.address,
-            category = searchEntity.category,
-            city = searchEntity.city,
-            phone = searchEntity.phone
-        )
-    }
+fun SearchEntity.toSearchResponse(): SearchResponse {
+    return SearchResponse(
+        hits = this.hits.map { hit ->
+            SearchResponse.Hit(
+                city = hit.city,
+                country = hit.country,
+                countrycode = hit.countrycode,
+                extent = hit.extent.toList(),
+                name = hit.name,
+                osm_id = hit.osm_id,
+                osm_key = hit.osm_key,
+                osm_type = hit.osm_type,
+                osm_value = hit.osm_value,
+                point = SearchResponse.Hit.Point(
+                    lat = hit.point.lat,
+                    lng = hit.point.lng
+                ),
+                state = hit.state
+            )
+        },
+        locale = this.locale
+    )
+}
