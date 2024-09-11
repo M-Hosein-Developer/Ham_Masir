4c0a9f561867e80f32a387903763464ef62124b1
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-09-11 04:36:06 -1200

    Setup Local Database For Search

--- data/src/main/java/ir/androidcoder/data/local/entities/SearchEntity.kt
+++ data/src/main/java/ir/androidcoder/data/local/entities/SearchEntity.kt
@@ -2,19 +2,22 @@ package ir.androidcoder.data.local.entities
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import java.net.IDN
 
 @Entity
 data class SearchEntity(
 
-    @PrimaryKey(autoGenerate = true)
-    val id : Int ,
-    val latitude : Double,
-    val longitude : Double,
-    val name : String,
-    val address : String,
-    val city : String,
-    val category : String,
-    val phone : String,
+    @PrimaryKey(autoGenerate = false)
+    val osm_id: Long?,
+    val city: String?,
+    val country: String?,
+    val countrycode: String?,
+    val extent: List<Double>?,
+    val name: String?,
+    val osm_key: String?,
+    val osm_type: String?,
+    val osm_value: String?,
+    val state: String?,
+    val lat: Double?,
+    val lng: Double
 
 )
