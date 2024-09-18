6e706b448f9c471e588c40a0827b87265c02e2d0
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-09-17 23:44:34 -1200

    Create Nav Bottom Bar

--- presentation/src/main/java/ir/androidcoder/hammasir/screen/MainActivity.kt
+++ presentation/src/main/java/ir/androidcoder/hammasir/screen/MainActivity.kt
@@ -1,5 +1,6 @@
 package ir.androidcoder.hammasir.screen
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,15 +11,25 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import dagger.hilt.android.AndroidEntryPoint
+import ir.androidcoder.hammasir.R
 import ir.androidcoder.hammasir.screen.feature.MapScreen
 import ir.androidcoder.hammasir.screen.feature.SearchScreen
 import ir.androidcoder.hammasir.screen.ui.theme.HamMasirTheme
@@ -45,29 +56,95 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun MyScreen(mapViewModel: MapViewModel , searchViewModel: SearchViewModel) {
 
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = MyScreen.MapScreen.route + "/{latLocation}/{longLocation}/{name}") {
 
-        composable(
-            route = MyScreen.MapScreen.route + "/{latLocation}/{longLocation}/{name}",
-            arguments = listOf(navArgument("latLocation") {type = NavType.StringType} , navArgument("longLocation") {type = NavType.StringType}  , navArgument("name") {type = NavType.StringType}),
-            enterTransition = { slideInHorizontally(initialOffsetX = { 500 }) + fadeIn() },
-            exitTransition = { slideOutHorizontally(targetOffsetX = { -500 }) + fadeOut() },
-            ){
-            MapScreen(mapViewModel , navController , searchViewModel , it.arguments?.getString("latLocation") , it .arguments?.getString("longLocation") , it .arguments?.getString("name"))
+    Scaffold(
+        bottomBar = {
+            NavigationBar {
+
+                NavigationBarItem(
+                    selected = false,
+                    onClick = { },
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Outlined.Settings,
+                            contentDescription = null
+                        )
+                    },
+                    label = {
+                        Text(text = "تنظیمات")
+                    }
+                )
+
+                NavigationBarItem(
+                    selected = false,
+                    onClick = { },
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Outlined.Search,
+                            contentDescription = null
+                        )
+                    },
+                    label = {
+                        Text(text = "پیدا کردن")
+                    }
+                )
+
+                NavigationBarItem(
+                    selected = false,
+                    onClick = { },
+                    icon = {
+                        Icon(
+                            painter = painterResource(R.drawable.finding_map),
+                            contentDescription = null
+                        )
+                    },
+                    label = {
+                        Text(text = "نقشه")
+                    }
+                )
+
+            }
+
         }
+    ) {
 
-        composable(
-            route = MyScreen.SearchScreen.route,
-            ){
-            SearchScreen(navController , searchViewModel)
+        NavHost(
+            navController = navController,
+            startDestination = MyScreen.MapScreen.route + "/{latLocation}/{longLocation}/{name}"
+        ) {
+
+            composable(
+                route = MyScreen.MapScreen.route + "/{latLocation}/{longLocation}/{name}",
+                arguments = listOf(navArgument("latLocation") { type = NavType.StringType },
+                    navArgument("longLocation") { type = NavType.StringType },
+                    navArgument("name") { type = NavType.StringType }),
+                enterTransition = { slideInHorizontally(initialOffsetX = { 500 }) + fadeIn() },
+                exitTransition = { slideOutHorizontally(targetOffsetX = { -500 }) + fadeOut() },
+            ) {
+                MapScreen(
+                    mapViewModel,
+                    navController,
+                    searchViewModel,
+                    it.arguments?.getString("latLocation"),
+                    it.arguments?.getString("longLocation"),
+                    it.arguments?.getString("name")
+                )
+            }
+
+            composable(
+                route = MyScreen.SearchScreen.route,
+            ) {
+                SearchScreen(navController, searchViewModel)
+            }
+
         }
 
     }
-
 
 }
 
