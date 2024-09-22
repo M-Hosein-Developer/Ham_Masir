8483c71b92b3d021f2aff89b16f6c502d4a56ffc
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-09-21 19:53:37 -1200

    Debugging  Profile In Setting Screen

--- presentation/src/main/java/ir/androidcoder/hammasir/screen/feature/SettingScreen.kt
+++ presentation/src/main/java/ir/androidcoder/hammasir/screen/feature/SettingScreen.kt
@@ -1,29 +1,35 @@
 package ir.androidcoder.hammasir.screen.feature
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.KeyboardArrowLeft
-import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import ir.androidcoder.hammasir.R
 
 @Composable
 fun SettingScreen(){
 
     Column(
-        Modifier.fillMaxSize()
+        Modifier
+            .fillMaxSize()
     ) {
 
         HamMasirProfile()
@@ -36,37 +42,34 @@ fun SettingScreen(){
 fun HamMasirProfile(){
 
     Row(
-        Modifier.fillMaxWidth(),
+        Modifier.fillMaxWidth().background(Color.Gray).padding(top = 42.dp),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.End
     ) {
 
-        AsyncImage(
-            model = Icons.Outlined.Person,
-            contentDescription = null,
-            modifier = Modifier.size(70.dp)
-            )
-
         Column(
-            Modifier.fillMaxWidth()
+            Modifier
+                .padding(horizontal = 16.dp),
+            horizontalAlignment = Alignment.End
         ) {
 
-            Text(text = "کاربر نشان")
+            Text(text = "کاربر نشان" , Modifier.padding(top = 8.dp))
 
-            Text(text = "09107806365")
+            Text(text = "09107806365" , Modifier.padding(top = 6.dp))
 
             TextButton(onClick = { /*TODO*/ }) {
-                Text(text = "مشاهده پروفایل")
                 Icon(imageVector = Icons.AutoMirrored.Outlined.KeyboardArrowLeft, contentDescription = null)
+                Text(text = "مشاهده پروفایل" , style = TextStyle(textAlign = TextAlign.End))
             }
         }
 
+        AsyncImage(
+            model = Icon(painter = painterResource(R.drawable.user), contentDescription = null),
+            contentDescription = null,
+            modifier = Modifier
+                .size(20.dp)
+        )
+
     }
 
-}
-
-@Composable
-@Preview(showBackground = true)
-fun SettingScreenPreview() {
-    SettingScreen()
 }
